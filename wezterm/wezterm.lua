e995f63cd63d6a4aa9a864d1bbd9dcd2befd8521
kai <kai@kai.attlocal.net>
2024-11-24 02:22:10 -0500

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -17,19 +17,21 @@ return {
 	cursor_blink_ease_in = "Constant",
 	cursor_blink_ease_out = "Constant",
 	window_background_opacity = 0.75,
-	colors = {
-		cursor_bg = "#A6ACCD",
-		cursor_border = "#A6ACCD",
-		cursor_fg = "#1B1E28",
-		tab_bar = {
-			background = "#4c4f69",
-		},
-	},
+	--	colors = {
+	--		cursor_bg = "#A6ACCD",
+	--		cursor_border = "#A6ACCD",
+	--		cursor_fg = "#1B1E28",
+	--		tab_bar = {
+	--			background = "#4c4f69",
+	--		},
+	--	},
 	use_fancy_tab_bar = false,
 	tab_bar_at_bottom = true,
 	tab_max_width = 999999,
 	leader = mappings.leader,
 	keys = mappings.keys,
 	key_tables = mappings.key_tables,
-	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
+	--	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
+	--	color_scheme = "Catppuccin Macchiato",
+	color_scheme = "Gruvbox Dark (Gogh)",
 }
